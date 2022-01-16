59f617aeaa31f69f6afd5237807d811ef2744e41
vgeroim <vgeroim.qa@gmail.com>
2022-01-15 20:39:17 -0600

    add url and basic assertion tests

--- cypress/integration/examples/simple.spec.js
+++ cypress/integration/examples/simple.spec.js
@@ -1,11 +1,20 @@
-//const { describe } = require("mocha");
 
-describe("Group 1", () => {
-    it("Should validate true", () => {
-        expect(true).to.equal(true);
+//Notes: keep under 10 tests in a describe block to simplify the debugging process.
+describe('Load URL, Assertions', () => {
+    it('should wait to load the page only during 10 seconds', () => {
+        cy.visit("https://example.com", { timeout: 10000 }); //It fails after 10 sec if page won't be loaded
     });
 
-    it("Should fail the test", () => {
-        expect(true).to.equal(false);
-    })
+    it('should validate URL name', () => {
+        cy.url().should('include', 'example.com');//Validates the URL name
+    });
+
+    it('the main title should be visible', () => {
+        cy.get('h1').should('be.visible');//Validates the element is present
+    });
+});
+
+//Cypress has automatic implicit wait by default
+describe('Implicit Waits, Pause and Debug', () => {
+
 });
