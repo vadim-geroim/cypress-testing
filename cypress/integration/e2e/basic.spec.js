7e3a4b55f2c870d45a4fa98e0eb9e8f746ff1271
vgeroim <vgeroim.qa@gmail.com>
2022-01-18 07:09:07 -0600

    add test scenario with Navbar navigation

--- cypress/integration/e2e/basic.spec.js
+++ cypress/integration/e2e/basic.spec.js
@@ -9,7 +9,7 @@ describe('Searchbox', () => {
     });
 });
 
-describe.only('Send forgotten password', () => {
+describe('Send forgotten password', () => {
     before(function () {
         cy.visit('http://zero.webappsecurity.com/index.html');
     });
@@ -19,5 +19,23 @@ describe.only('Send forgotten password', () => {
         cy.get('a').contains('Forgot your password ?').click();
         cy.get('#user_email').type('some email');
         cy.get('input[value="Send Password"]').click();
+    });
+});
+
+describe.only('Navbar links validation', () => {
+    beforeEach(function () {
+        cy.visit('http://zero.webappsecurity.com/index.html');
+    });
+
+    it('should load Onlin Banking page', () => {
+        cy.get('#onlineBankingMenu').click();
+        cy.title().should('eq', 'Zero - Free Access to Online Banking');
+        cy.url().should('include', '/online-banking.html');
+    });
+
+    it('should load Feedback page', () => {
+        cy.get('#feedback').click();
+        cy.title().should('include', 'Zero - Contact Us');
+        cy.url().should('include', 'feedback.html');
     });
 });
