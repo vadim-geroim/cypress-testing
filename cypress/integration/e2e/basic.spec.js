a2ca8874c660b5e503b3f4a69c1f44c7da5a446d
vgeroim <vgeroim.qa@gmail.com>
2022-01-18 19:19:26 -0600

    add test scenarios with Payment, Currency Exchange, Transfer Funds and Filter Transactions

--- cypress/integration/e2e/basic.spec.js
+++ cypress/integration/e2e/basic.spec.js
@@ -85,7 +85,7 @@ describe('Login / logout flow', () => {
     });
 });
 
-describe.only('Craete new payee', () => {
+describe('Craete new payee', () => {
     //User login
     before(function () {
         cy.visit('http://zero.webappsecurity.com/index.html');
@@ -109,5 +109,107 @@ describe.only('Craete new payee', () => {
         cy.get('#add_new_payee').click();
         //Validation
         cy.get('#alert_content').should('be.visible').and('contain', 'The new payee fake name was successfully created.');
+    });
+});
+
+describe('Payment test', () => {
+    //User login
+    before(function () {
+        cy.visit('http://zero.webappsecurity.com/index.html');
+        cy.get('#signin_button').click();
+        cy.fixture('user-data').then(user => {
+            cy.get('#user_login').clear().type(user.name);
+            cy.get('#user_password').clear().type(user.password);
+            cy.get('input[value="Sign in"]').click();
+        });
+    });
+
+    it('should send (fake) new payment', () => {
+        cy.get('#pay_bills_tab a').click();
+        cy.contains('Make payments to your saved payees');
+        cy.get('#sp_payee').select('Wells Fargo');
+        cy.get('#sp_account').select('Brokerage');
+        cy.get('#sp_amount').clear().type(0);
+        cy.get('#sp_date').type('1590-01-01 {enter}');
+        cy.get('#sp_description').clear().type('description');
+        cy.get('#pay_saved_payees').click();
+        cy.get('#alert_content').should('be.visible').and('contain', 'The payment was successfully submitted.');
+    });
+});
+
+describe('Currency Exchange', () => {
+    //User login
+    before(function () {
+        cy.visit('http://zero.webappsecurity.com/index.html');
+        cy.get('#signin_button').click();
+        cy.fixture('user-data').then(user => {
+            cy.get('#user_login').clear().type(user.name);
+            cy.get('#user_password').clear().type(user.password);
+            cy.get('input[value="Sign in"]').click();
+        });
+    });
+
+    it('should display the currency exchange amount', () => {
+        cy.get('#pay_bills_tab a').click();
+        cy.get('a[href="#ui-tabs-3"]').click();
+        cy.get('#pc_currency').select('China (yuan)');
+        cy.get('#pc_amount').clear().type(100);
+        cy.get('#pc_inDollars_true').click();
+        cy.get('#pc_calculate_costs').click();
+        cy.get('#pc_conversion_amount').should('contain', '575.71 yuan (CNY) = 100.00 U.S. dollar (USD)');
+    });
+});
+
+describe('Transfer Funds', () => {
+    //User login
+    before(function () {
+        cy.visit('http://zero.webappsecurity.com/index.html');
+        cy.get('#signin_button').click();
+        cy.fixture('user-data').then(user => {
+            cy.get('#user_login').clear().type(user.name);
+            cy.get('#user_password').clear().type(user.password);
+            cy.get('input[value="Sign in"]').click();
+        });
+    });
+
+    it('should verify entered data', () => {
+        cy.get('#transfer_funds_tab a').click();
+        //Fill in the form
+        cy.get('#tf_fromAccountId').select('1');
+        cy.get('#tf_toAccountId').select('2');
+        cy.get('#tf_amount').clear().type(50);
+        cy.get('#tf_description').clear().type('desc');
+        cy.get('#btn_submit').click();
+        //Validation
+        cy.get('#tf_fromAccountId').should('have.value', 'Savings');
+        cy.get('#tf_toAccountId').should('have.value', 'Checking');
+        cy.get('#tf_amount').should('have.value', '50');
+        cy.get('#tf_description').should('have.value', 'desc');
+    })
+});
+
+describe('Filter Transactions', () => {
+    //User login
+    before(function () {
+        cy.visit('http://zero.webappsecurity.com/index.html');
+        cy.get('#signin_button').click();
+        cy.fixture('user-data').then(user => {
+            cy.get('#user_login').clear().type(user.name);
+            cy.get('#user_password').clear().type(user.password);
+            cy.get('input[value="Sign in"]').click();
+        });
+    });
+
+    it('should validate filter result', () => {
+        //Navigate to filter
+        cy.get('#account_activity_tab').click();
+        cy.get('a[href="#ui-tabs-2"]').click();
+        //Fill in filter
+        cy.get('#aa_fromAmount').clear().type(0);
+        cy.get('#aa_toAmount').clear().type(100);
+        cy.get('button').contains('Find').click();
+        //Validation
+        cy.get('#filtered_transactions_for_account').should('be.visible');
+        cy.get('tbody > tr').its('length').should('be.gt', 0);
     });
 });
